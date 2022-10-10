7597881de0fa903937575e5081a832074d4e417a
akenaaa <aruzhan.tolegen@jihc.kz>
2022-10-10 20:32:09 +0600

    First commit

---
+++ collections.java
@@ -0,0 +1,194 @@
+//
+//import java.util.ArrayList;
+//
+//public class collections {
+//    public static void main(String[] args) {
+//        ArrayList<String> cars =new ArrayList<String>();
+//        cars.add("Cocie");
+//        cars.add("Chocolate");
+//        cars.add("Coco");
+//        cars.add("pupil");
+//        System.out.println(cars);
+//
+//        String car = cars.get(0);
+//        System.out.println("get method :" + car);
+//
+//        cars.set(0,"Opel");
+//        System.out.println("set method :" +cars);
+//
+//        cars.remove(0);
+//        System.out.println("set method :" +cars);
+//
+//        cars.clear();
+//        System.out.println("set method :" +cars);
+//
+//        int s = cars.size();
+//        System.out.println(" list size  :" +s);
+//    }
+//}
+//
+//
+
+
+
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections{
+//    public static void main(String[] args){
+//        ArrayList<String>  colors = new ArrayList<>();
+//
+//        colors.add("lilac");
+//        colors.add("yellow");
+//        colors.add("blue");
+//        colors.add("black");
+//
+//        Collections.sort(colors);
+//        System.out.println(colors);
+//    }
+//}
+
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections{
+//    public static void main(String[] args){
+//        ArrayList<String>  colors = new ArrayList<>();
+//
+//        colors.add("lilac");
+//        colors.add("yellow");
+//        colors.add("blue");
+//        colors.add("black");
+//        colors.add(0, "white");
+//        System.out.println(colors);
+//    }
+//}
+
+
+// public class collections {
+//     public static void main(String[] args) {
+//         ArrayList<String> colors = new ArrayList<String>();
+//         colors.add("lilac");
+//         colors.add("yellow");
+//         colors.add("blue");
+//         colors.add("black");
+//         String color = colors.get(2);
+//         System.out.println(color);
+//     }
+// }
+
+
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections{
+//    public static void main(String[] args){
+//        ArrayList<String> colors = new ArrayList<>();
+//
+//        colors.add("lilac");
+//        colors.add("yellow");
+//        colors.add("blue");
+//        colors.add("black");
+//        colors.set(2, "white");
+//        System.out.println(colors);
+//    }
+//}
+
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections{
+//    public static void main(String[] args){
+//        ArrayList<String> colors = new ArrayList<>();
+//
+//        colors.add("lilac");
+//        colors.add("yellow");
+//        colors.add("blue");
+//        colors.add("black");
+//        colors.remove(2);
+//        System.out.println(colors);
+//    }
+//}
+
+
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections{
+//    public static void main(String[] args){
+//        ArrayList<String> colors = new ArrayList<>();
+//
+//        colors.add("lilac");
+//        colors.add("yellow");
+//        colors.add("blue");
+//        colors.add("black");
+//
+//        System.out.println(colors.indexOf("lilac"));
+//    }
+//}
+
+
+// import java.sql.SQLOutput;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections{
+//    public static void main(String[] args){
+//        ArrayList<String> colors = new ArrayList<>();
+//
+//        colors.add("lilac");
+//        colors.add("yellow");
+//        colors.add("blue");
+//        colors.add("black");
+//
+//        ArrayList<String> car = new ArrayList<>();
+//        car = colors;
+//        System.out.println(colors);
+//        System.out.println(car);
+//
+//
+//    }
+//}
+
+
+
+// public class collections{
+//     public static void main(String[] args) {
+//         ArrayList<String> colors = new ArrayList<String>();
+//         colors.add("lilac");
+//         colors.add("yellow");
+//         colors.add("blue");
+//         colors.add("black");
+//         colors.add("white");
+//         colors.add("orange");Collections.shuffle(colors);
+//         System.out.println(colors);
+//     }
+// }
+
+
+
+
+//import java.sql.SQLOutput;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class collections {
+//    public static void main(String[] args) {
+//        ArrayList<String> colors = new ArrayList<>();
+//
+//        colors.add("blue");
+//        colors.add("yellow");
+//        colors.add("white");
+//        colors.add("black");
+//
+//        Collections.reverse(colors);
+//        System.out.println(colors);
+//
+//    }
+//}
+
